f9954c317109c682c395b9eebba49f3fbcfbfe54
Adrien Leloup <contact@void.graphics>
2017-08-11 17:43:22 +0200

    Transfer input events to parent

--- src/modules/groupfield.vue
+++ src/modules/groupfield.vue
@@ -2,13 +2,19 @@
     <div class="field group">
         <label class="field__label">{{ label }}</label>
         <div class="field__container">
-            <genericfield v-for="(field, index) in options" :value="values[index]" :structure="field" :key="field.label"></genericfield>
+            <genericfield v-for="(field, index) in options" :value="values[index]" :structure="field" @input="transferInput($event, index)" :key="index"></genericfield>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['label', 'options', 'values']
+    props: ['label', 'options', 'values'],
+
+    methods: {
+        transferInput(value, index) {
+            this.$emit('input', {value, index});
+        }
+    }
 }
 </script>
