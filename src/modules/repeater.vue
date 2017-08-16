940aacd9acb01df21ace64c86b1156d547b0f414
Adrien Leloup <contact@void.graphics>
2017-08-16 15:18:42 +0200

    Added ability to cancel changes

--- src/modules/repeater.vue
+++ src/modules/repeater.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="field repeater" :class="classes" :id="id">
         <label class="field__label">{{ label }}</label>
-        <div class="field__container" :style="{ 'min-height': minheight }">
+        <div class="field__container">
             <transition mode="out-in" @after-leave="showfields = true" name="slide">
-            <draggable class="repeater__items" :options="{ animation: 300 }" v-show="!current && current !== 0" v-model="list" @end="cancel">
+            <draggable class="repeater__items" :options="{ animation: 300 }" v-show="!current && current !== 0" v-model="list" @end="cancelDelete">
                 <div class="repeater__item" v-for="(item, index) in list" :key="index">
                     <p class="repeater__title">
                         <em class="repeater__number">#{{ index + 1 }}</em>
@@ -11,7 +11,7 @@
                     </p>
                     <a @click.prevent="remove(index)" v-if="!confirmdelete[index]" class="repeater__control repeater__control--delete">{{ trans('fields.repeater.delete') }}</a>
                     <a v-if="confirmdelete[index]" class="repeater__control repeater__control--destroy" @click.prevent="destroy(index)">{{ trans('fields.repeater.confirmdelete') }}</a>
-                    <a v-if="confirmdelete[index]" class="repeater__control repeater__control--cancel" @click.prevent="cancel(index)">{{ trans('fields.repeater.cancel') }}</a>
+                    <a v-if="confirmdelete[index]" class="repeater__control repeater__control--cancel" @click.prevent="cancelDelete(index)">{{ trans('fields.repeater.cancel') }}</a>
                     <a @click.prevent="edit(index)" class="repeater__control repeater__control--edit">{{ trans('fields.repeater.edit') }}</a>
                 </div>
             </draggable>
@@ -28,7 +28,7 @@
             <div class="field__footer">
                 <p class="field__tip">{{ tip }}</p>
                 <div class="field__actions">
-                    <a v-if="current || current == 0" @click.prevent="finish" class="field__action btn btn--tiny">{{ trans('fields.repeater.cancel') }}</a>
+                    <a v-if="current || current == 0" @click.prevent="cancel" class="field__action btn btn--tiny">{{ trans('fields.repeater.cancel') }}</a>
                     <a v-if="current || current == 0" @click.prevent="finish" class="field__action btn btn--tiny btn--secondary">{{ trans('fields.repeater.finish') }}</a>
                     <a v-else @click.prevent="add" class="field__action btn btn--tiny btn--secondary">{{ trans('fields.repeater.add') }}</a>
                 </div>
@@ -42,9 +42,7 @@ import draggable from 'vuedraggable';
 
 /*
 Todo: 
-Cancel changes
-Cancel delete
-Investigate first drag and drop not registering
+Investigate reference issues with this.list to avoid having to do a snapshot on created()
 */
 
 export default {
@@ -56,14 +54,15 @@ export default {
             current: null,
             showfields: false,
             id: this._uid,
-            minheight: 0,
-            list: [],
-            confirmdelete: []
+            list: this.items,
+            confirmdelete: [],
+            restorepoint: null
         }
     },
 
     created() {
-        this.list = this.items;
+        this.snapshot();
+        this.restore();
     },
 
     methods: {
@@ -95,14 +94,21 @@ export default {
             delete this.confirmdelete[index];
         },
 
-        cancel(index) {
+        cancelDelete(index) {
             if(typeof index == 'int') delete this.confirmdelete[index];
             else this.confirmdelete = [];
             this.$forceUpdate();
         },
 
         edit(index) {
+            this.snapshot();
             this.current = index;
+        },
+
+        cancel() {
+            this.restore();
+            this.current = null;
+            this.showfields = false;
         },
 
         finish() {
@@ -110,14 +116,15 @@ export default {
         },
 
         add() {
-            let preview = '';
-            if(this.structure.type == 'group') preview = this.getPreviewObject();
-            else preview = '';
-            this.list.push(preview);
-            this.edit(this.list.length - 1);
+            this.snapshot();
+            let blank = '';
+            if(this.structure.type == 'group') blank = this.getBlank();
+            else blank = '';
+            this.list.push(blank);
+            this.current = this.list.length - 1;
         },
 
-        getPreviewObject() {
+        getBlank() {
             let preview = {};
             for(let key in this.structure.options) {
                 preview[key] = '';
@@ -141,12 +148,25 @@ export default {
             } else {
                 this.list[index] = e;
             }
+        },
+
+        snapshot() {
+            this.restorepoint = JSON.parse(JSON.stringify(this.list));
+        },
+
+        restore() {
+            console.log('old', this.list);
+            console.log('restore', this.restorepoint);
+            this.list = this.restorepoint;
+            this.restorepoint = null;
         }
     },
 
     computed: {
         tip() {
-            return this.current ? this.trans('fields.repeater.editTip') : this.list.length ? this.trans('fields.repeater.tip') : this.trans('fields.repeater.empty');
+            return this.current ? this.trans('fields.repeater.editTip') 
+                   : this.list.length ? this.trans('fields.repeater.tip') 
+                   : this.trans('fields.repeater.empty');
         },
 
         classes() {
