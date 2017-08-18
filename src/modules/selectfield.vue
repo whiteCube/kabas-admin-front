da420c04eb8e9a9d26a016754ed189e50af30686
Adrien Leloup <contact@void.graphics>
2017-08-18 12:43:50 +0200

    renamed items to options

--- src/modules/selectfield.vue
+++ src/modules/selectfield.vue
@@ -4,12 +4,12 @@
         <div class="field__container">
             <select v-model="selected" :name="name" :id="id" class="sro">
                 <option value="">{{ this.trans('fields.select.prompt') }}</option>
-                <option :value="key" v-for="(item, key) in items">{{ item }}</option>
+                <option :value="key" v-for="(item, key) in options">{{ item }}</option>
             </select>
-            <p class="field__element" @click="expanded = !expanded">{{ selected ? items[selected] : trans('fields.select.prompt') }}</p>
+            <p class="field__element" @click="expanded = !expanded">{{ selected ? options[selected] : trans('fields.select.prompt') }}</p>
             <transition name="slideDown">
             <div v-if="expanded" class="select__dropdown">
-                <a v-for="(item, index) in items" @click.prevent="selectItem(index)" class="select__option">
+                <a v-for="(item, index) in options" @click.prevent="selectItem(index)" class="select__option">
                     {{ item }}
                 </a>
             </div>
@@ -20,7 +20,7 @@
 
 <script>
 export default {
-    props: ['label', 'name', 'items', 'value'],
+    props: ['label', 'name', 'options', 'value'],
 
     data() {
         return {
