a9d6abc9061cfa2d039637503b175963ea02c3a2
Adrien Leloup <contact@void.graphics>
2017-08-30 17:50:34 +0200

    Added arrow and show dropdown on click

--- src/modules/urlfield.vue
+++ src/modules/urlfield.vue
@@ -3,7 +3,7 @@
         <label class="field__label" :for="id">{{ label }}</label>
         <div class="field__container">
             <a class="url__switcher" @click.prevent="switchType">{{ trans('fields.url.' + type) }}</a>
-            <input type="text" class="field__element" :id="id" v-model="url" :name="name" :placeholder="placeholder" :value="value" @input="autocomplete" @blur="expanded = false">
+            <input type="text" class="field__element" :id="id" v-model="url" :name="name" :placeholder="placeholder" :value="value" @focus="autocomplete" @input="autocomplete" @blur="expanded = false">
             <transition name="slideDown">
             <div v-if="expanded" class="select__dropdown">
                 <a v-for="(item, index) in items" @click.prevent="selectItem(index)" class="select__option">
@@ -62,9 +62,14 @@ export default {
             this.expanded = false;
         },
 
+        showifinternal() {
+            if(this.type != 'internal') return;
+            this.expanded = true;
+        },
+
         autocomplete(e) {
             this.$emit('input', e.target.value);
-            if(this.type == 'external' || !this.url.length) return this.expanded = false;
+            if(this.type == 'external') return this.expanded = false;
             this.query();
         }
     },
@@ -72,7 +77,8 @@ export default {
     computed: {
         classes() {
             return {
-                'url--expanded': this.expanded
+                'url--expanded': this.expanded,
+                'url--internal': this.type == 'internal'
             }
         }
     }
