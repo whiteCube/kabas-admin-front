436e1cdf6f80cea5b1eb875dfc46f7c287b0ade5
Adrien Leloup <contact@void.graphics>
2017-08-31 16:48:48 +0200

    Removed old code

--- src/mixins/repeatable.js
+++ src/mixins/repeatable.js
@@ -5,17 +5,12 @@ export default {
 
     data() {
         return {
-            level: 0,
-            height: '75px',
-            animating: false
+            level: 0
         }
     },
 
     created() {
         this.level = this.hasProp('nestinglevel') ? this.nestinglevel : 0;
-
-        EventBus.$on('resize', this.resize);
-        EventBus.$on('initialsize', this.initialSize);
     },
 
     methods: {
@@ -26,46 +21,6 @@ export default {
             } 
             if(!this.$refs[index]) return;
             this.$refs[index][0].hideRecursivelyUntil(level);
-        },
-
-        calculateHeight() {
-            let height = 0;
-            let container = this.$el.querySelector('.field__container');
-            for(let i = 0; i < container.children.length; i++) {
-                if(this.isExcluded(container.children[i])) continue;
-                let el = container.children[i].cloneNode(true);
-                el.style.display = 'block';
-                container.appendChild(el);
-                height += el.clientHeight;
-                container.removeChild(el);
-            }
-            return height + 4 + 'px';
-        },
-
-        initialSize(parent, height) {
-            if(parent != this.id) return;
-            this.height = height + 75 + 'px' || this.calculateHeight();
-            this.animating = true;
-        },
-
-        resize(parent) {
-            if(parent != this.id) return;
-            this.height = this.calculateHeight();
-            setTimeout(() => {
-                this.animating = false;
-            }, 200);
-        },
-
-        isExcluded(el) {
-            return (this.showfields && el.className == 'repeater__items') ||
-                   (!this.showfields && el.className == 'repeater__editable');
-        }
-    },
-
-    computed: {
-        calculatedHeight() {
-            if(!this.animating) return '';
-            return this.height;
         }
     }
 }
