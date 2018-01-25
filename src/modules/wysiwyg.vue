45ba92da045d1e58cd69a43753e7176ea9e43c3b
Adrien Leloup <adrien@whitecube.be>
2018-01-25 17:20:35 +0100

    Changed wysiwyg editor

--- src/modules/wysiwyg.vue
+++ src/modules/wysiwyg.vue
@@ -2,13 +2,15 @@
     <div class="field wysiwyg">
         <label class="field__label" @click="focus" :for="id">{{ label }}</label>
         <div class="field__container">
-            <textarea class="field__element" :id="id" v-model="val" :name="computedName"></textarea>
+            <div :id="id + 'tui'"></div>
+            <textarea class="field__element sro" :id="id" v-model="val" :name="computedName"></textarea>
         </div>
     </div>
 </template>
 
 <script>
 import SimpleMDE from 'SimpleMDE';
+import Editor from 'tui-editor';
 import EventBus from '../mixins/event-bus.js';
 
 export default {
@@ -17,34 +19,32 @@ export default {
         return {
             id: this._uid,
             element: null,
-            mde: null,
-            val: this.value
+            val: this.value,
+            editor: null
         }
     },
 
     methods: {
         focus() {
-            this.mde.codemirror.focus();
+            this.editor.focus();
         }
     },
 
     mounted() {
-        this.element = document.getElementById(this._uid);
-        this.mde = new SimpleMDE({
-            element: this.element,
-            placeholder: this.placeholder,
-            spellChecker: false,
-            status: false
+        this.element = document.getElementById(this._uid + 'tui');
+        this.editor = new Editor({
+            el: this.element,
+            initialEditType: 'wysiwyg',
+            previewStyle: 'vertical',
+            initialValue: this.value,
+            height: 'auto',
+            events: {
+                change: (e) => {
+                    this.val = this.editor.getValue();
+                    this.$emit('input', this.val);
+                }
+            }
         });
-
-        this.mde.codemirror.on('change', () => {
-            this.val = this.mde.value();
-            this.$emit('input', this.val);
-        });
-
-        // This fixes the issue with codemirror (wysiwyg) where the initial value does not appear right away.
-        // Can be removed safely the day we stop using simplemde.
-        EventBus.$on('updateWysiwyg', () => this.mde.codemirror.refresh());
     }
 }
 </script>
