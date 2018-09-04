b5130a1cc81b40c8c350ff6a0a1468d9692c43ff
Adrien Leloup <adrien@whitecube.be>
2018-09-04 16:07:08 +0200

    Added youtube plugin to wysiwyg editor

--- src/modules/wysiwyg.vue
+++ src/modules/wysiwyg.vue
@@ -16,6 +16,24 @@
 import Editor from 'tui-editor';
 import EventBus from '../mixins/event-bus.js';
 
+// define youtube extension
+Editor.defineExtension('youtube', function() {
+// runs while markdown-it transforms code block to HTML
+Editor.codeBlockManager.setReplacer('youtube', function(youtubeId) {
+    // Indentify multiple code blocks
+    var wrapperId = 'yt' + Math.random().toString(36).substr(2, 10);
+    // avoid sanitizing iframe tag
+    setTimeout(renderYoutube.bind(null, wrapperId, youtubeId), 0);
+        return '<div id="' + wrapperId + '"></div>';
+    });
+});
+
+function renderYoutube(wrapperId, youtubeId) {
+    console.log('render youtube', wrapperId, youtubeId);
+    var el = document.querySelector('#' + wrapperId);
+    el.innerHTML = '<iframe width="420" height="315" src="https://www.youtube.com/embed/' + youtubeId + '"></iframe>';
+}
+
 export default {
     props: ['label', 'name', 'limit', 'placeholder', 'value', 'position', 'description'],
     data() {
@@ -41,6 +59,7 @@ export default {
             previewStyle: 'vertical',
             initialValue: this.value,
             height: 'auto',
+            exts: ['youtube'],
             events: {
                 change: (e) => {
                     this.val = this.editor.getValue();
